d2762597631820a8cd71ed96f05884d3ca42ea21
Joshua Gugun Siagian <suabahasa@gmail.com>
2023-01-30 17:50:51 +0800

    use static prefix for php-scoper

--- deploy/scoper.inc.php
+++ deploy/scoper.inc.php
@@ -22,7 +22,8 @@ return [
     // will be generated instead.
     //
     // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#prefix
-    'prefix' => '_Yabe' . $timestamp,
+    // 'prefix' => '_Yabe' . $timestamp,
+    'prefix' => '_YabeWebfont',
 
     // By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
     // directory. You can however define which files should be scoped by defining a collection of Finders in the
