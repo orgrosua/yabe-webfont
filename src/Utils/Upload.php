6dcf17806d1d50a038ee52e46ed980cfa1998980
Joshua Gugun Siagian <suabahasa@gmail.com>
2023-03-11 16:19:10 +0800

    add filter hook for proxy the remote download

--- src/Utils/Upload.php
+++ src/Utils/Upload.php
@@ -87,6 +87,8 @@ class Upload
     {
         require_once(ABSPATH . 'wp-admin/includes/file.php');
 
+        $file_url = apply_filters('f!yabe/webfont/utils/upload:remote_upload_media:file_url', $file_url);
+
         $temp_file = download_url($file_url);
 
         if (is_wp_error($temp_file)) {
