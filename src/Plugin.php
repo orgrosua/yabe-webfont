632c61025b8eeb2516bf639a4338ae10fa34b10b
Joshua Gugun Siagian <suabahasa@gmail.com>
2023-03-12 18:17:20 +0800

    Bump VERSION to 2.0.16

--- src/Plugin.php
+++ src/Plugin.php
@@ -32,12 +32,12 @@ final class Plugin
     /**
      * @var string
      */
-    public const VERSION = '2.0.15';
+    public const VERSION = '2.0.16-DEV';
 
     /**
      * @var int
      */
-    public const VERSION_ID = 20015;
+    public const VERSION_ID = 20016;
 
     /**
      * @var int
@@ -52,12 +52,12 @@ final class Plugin
     /**
      * @var int
      */
-    public const RELEASE_VERSION = 15;
+    public const RELEASE_VERSION = 16;
 
     /**
      * @var string
      */
-    public const EXTRA_VERSION = '';
+    public const EXTRA_VERSION = 'DEV';
 
     /**
      * Easy Digital Downloads Software Licensing integration wrapper.
