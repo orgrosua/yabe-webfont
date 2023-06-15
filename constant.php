a2db99500d452999f42046cbff1009c041edca08
Joshua Gugun Siagian <suabahasa@gmail.com>
2023-06-15 16:26:00 +0800

    Bump VERSION to 2.0.33
    fix workflow

--- constant.php
+++ constant.php
@@ -26,12 +26,12 @@ class YABE_WEBFONT
     /**
      * @var string
      */
-    public const VERSION = '2.0.32';
+    public const VERSION = '2.0.33-DEV';
 
     /**
      * @var int
      */
-    public const VERSION_ID = 20032;
+    public const VERSION_ID = 20033;
 
     /**
      * @var int
@@ -46,12 +46,12 @@ class YABE_WEBFONT
     /**
      * @var int
      */
-    public const RELEASE_VERSION = 32;
+    public const RELEASE_VERSION = 33;
 
     /**
      * @var string
      */
-    public const EXTRA_VERSION = '';
+    public const EXTRA_VERSION = 'DEV';
 
     /**
      * @var string
