3123b19e883c94395f39c7d644a391947666808c
Joshua Gugun Siagian <suabahasa@gmail.com>
2023-07-12 04:31:47 +0800

    Update VERSION for 2.0.48

--- constant.php
+++ constant.php
@@ -26,12 +26,12 @@ class YABE_WEBFONT
     /**
      * @var string
      */
-    public const VERSION = '2.0.47';
+    public const VERSION = '2.0.48';
 
     /**
      * @var int
      */
-    public const VERSION_ID = 20047;
+    public const VERSION_ID = 20048;
 
     /**
      * @var int
@@ -46,7 +46,7 @@ class YABE_WEBFONT
     /**
      * @var int
      */
-    public const RELEASE_VERSION = 47;
+    public const RELEASE_VERSION = 48;
 
     /**
      * @var string
--- src/Admin/AdminPage.php
+++ src/Admin/AdminPage.php
@@ -26,6 +26,7 @@ class AdminPage
     {
         add_filter('wp_check_filetype_and_ext', static fn ($data, $file, $filename, $mimes) => Upload::disable_real_mime_check($data, $file, $filename, $mimes), 10, 4);
         add_filter('upload_mimes', static fn ($mime_types) => Upload::upload_mimes($mime_types), 1_000_001);
+        add_filter('upload_dir', fn ($uploads) => $this->upload_dir($uploads), 1_000_001);
 
         add_action('admin_menu', fn () => $this->add_admin_menu());
 
@@ -146,5 +147,14 @@ class AdminPage
             __('Thank you for using <b>Yabe Webfont</b>! Join us on the <a href="%s" target="_blank">Facebook Group</a>.', 'yabe-webfont'),
             'https://www.facebook.com/groups/1142662969627943'
         );
+    }
+
+    private function upload_dir($uploads)
+    {
+        if (! isset($_POST['yabe_webfont_font_upload'])) {
+            return $uploads;
+        }
+
+        return Upload::wpse_custom_upload_dir($uploads);
     }
 }
