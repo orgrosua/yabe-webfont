0a0293471ddfe31c6b0afdf797c90bec98046d96
Joshua Gugun Siagian <suabahasa@gmail.com>
2023-09-13 19:29:44 +0700

    Update VERSION for 2.0.54

--- constant.php
+++ constant.php
@@ -26,12 +26,12 @@ class YABE_WEBFONT
     /**
      * @var string
      */
-    public const VERSION = '2.0.53';
+    public const VERSION = '2.0.54';
 
     /**
      * @var int
      */
-    public const VERSION_ID = 20053;
+    public const VERSION_ID = 20054;
 
     /**
      * @var int
@@ -46,7 +46,7 @@ class YABE_WEBFONT
     /**
      * @var int
      */
-    public const RELEASE_VERSION = 53;
+    public const RELEASE_VERSION = 54;
 
     /**
      * @var string
